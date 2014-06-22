e2ac882f332a7634bc43704b1827e323be6396b5
GunWoo Choi <6566gun@gmail.com>
2014-06-22 19:28:25 +0900

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lancet
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/lancet/core_test.clj
@@ -0,0 +1,7 @@
+(ns lancet.core-test
+  (:require [clojure.test :refer :all]
+            [lancet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
